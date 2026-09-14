2386de2ce7074fe8eea7dd62a7705ace95132e8e
dalekhya186-art <dalekhya186@gmail.com>
2026-09-15 00:55:06 +0530

    Add files via upload

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = "sqlite:///./finflow.db"
+
+print("DATABASE FILE:", os.path.abspath("finflow.db"))
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
